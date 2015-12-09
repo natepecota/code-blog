e276dcb905a5b40b6d34d458f9820b9b76abfd75
natepecota <nate.pecota@gmail.com>
2015-12-08 20:29:01 -0800

    Work in progress on assignemtn 7.

--- scripts/stats.js
+++ scripts/stats.js
@@ -1,87 +1,121 @@
+// //stats object
+// var stats = {};
+// var statData = '';
+// var authorList = [];
+//
+// //Ajax call
+// stats.getData = function () {
+//  $.getJSON('scripts/hackerIpsum.json', function(data) {
+//    statData = data;
+//  }).done(numberOfArticles);
+//   //  .done(numberOfAuthors);
+// };
+// function numberOfArticles() {
+//   return ('#stats').html('<p>Number of Articles ' + statData.length + '</p>');
+// }
+//
+// stats.getData();
+
+var stats = {};
+​
+$.get('scripts/hackerIpsum.json', function(data) {
+  stats.data = data; //this saves the entire json file as an array that belongs to your stats object, that you can access anywhere
+}).done(stats.numberOfArticles)
+  .done(stats.numberOfAuthors);
+};
+​
 $(function() {
-
-  // functional core
-
-  // returns a specified property from a collection of objects
-  // in ES2015, it's even shorter: return collection.map(e => e[property])
-  function pluck(property, collection) {
-    return collection.map(function(e){
-      return e[property];
-    });
-  }
-
-  // return an array with unique elements
-  // this isn't efficient for sorted arrays, but whatevs
-  function unique(collection) {
-    // ternary operator to guard for empty collection
-    var length = collection ? collection.length : 0;
-
-    if (!length) { return []; }
-
-    var seen = [];
-    collection.forEach(function(e) {
-      if ( seen.indexOf(e) < 0 ) { seen.push(e); }
-    });
-    return seen;
-  }
-
-  // compose two functions together
-  function compose(func1, func2) {
-    return function() {
-      return func1(func2.apply(null, arguments));
-    };
-  }
-
-  var distinct = compose(unique, pluck);
-
-  // TODO do something useful with reduce and filter
-
-  // jquery render functions (IO)
-  var $headline = $('<h1>Stats</h1>');
-
-  function $numberOfArticles(articles) {
-    return $('<p>Number of articles: ' + articles.length + '</p>');
-  }
-
-  function $numberOfAuthors(articles) {
-    var numAuthors = distinct('author', articles).length;
-    return $('<p>Number of authors: ' + numAuthors + '</p>');
-  }
-
-  function $numberOfWords(articles) {
-    var numWords = articles.split(' ');
-    return $('<p>Number of words: ' + numWords + '</p>');
-  }
-
-  var $statsComponent = function(blog) {
-    var component = $('<div>');
-    component.append([
-      $headline,
-      $numberOfArticles(blog),
-      $numberOfAuthors(blog),
-      // $numberOfWords(blog)
-    ]);
-    return component;
-  };
-
-  function renderStats(blog) {
-    $('#stats').replaceWith($statsComponent(blog));
-  }
-
-  function renderError(message, xhr) {
-    $('#stats').replaceWith($('<p>Error: <code>' + message + xhr + '</code></p>'));
-  }
-
-  // imperative shell
-
-  function stats(data, message, xhr) {
-    if (xhr.status != 200) {
-      renderError(message, xhr);
-    } else {
-      renderStats(data);
-    }
-  }
-
-  $.getJSON('scripts/hackerIpsum.json', stats);
-
+  stats.data.forEach( function(article) {
+    words = article.body.split(" ")
+	console.log(words)
+  })
 });
+
+// $(function() {
+//
+//   // functional core
+//
+//   // returns a specified property from a collection of objects
+//   // in ES2015, it's even shorter: return collection.map(e => e[property])
+//   function pluck(property, collection) {
+//     return collection.map(function(e){
+//       return e[property];
+//     });
+//   }
+//
+//   // return an array with unique elements
+//   // this isn't efficient for sorted arrays, but whatevs
+//   function unique(collection) {
+//     // ternary operator to guard for empty collection
+//     var length = collection ? collection.length : 0;
+//
+//     if (!length) { return []; }
+//
+//     var seen = [];
+//     collection.forEach(function(e) {
+//       if ( seen.indexOf(e) < 0 ) { seen.push(e); }
+//     });
+//     return seen;
+//   }
+//
+//   // compose two functions together
+//   function compose(func1, func2) {
+//     return function() {
+//       return func1(func2.apply(null, arguments));
+//     };
+//   }
+//
+//   var distinct = compose(unique, pluck);
+//
+//   // TODO do something useful with reduce and filter
+//
+//   // jquery render functions (IO)
+//   var $headline = $('<h1>Stats</h1>');
+//
+//   function $numberOfArticles(articles) {
+//     return $('<p>Number of articles: ' + articles.length + '</p>');
+//   }
+//
+//   function $numberOfAuthors(articles) {
+//     var numAuthors = distinct('author', articles).length;
+//     return $('<p>Number of authors: ' + numAuthors + '</p>');
+//   }
+//
+//   function $numberOfWords(articles) {
+//     var numWords = $.forEach()
+//     })
+//     return $('<p>Number of words: ' + numWords + '</p>');
+//   }
+//
+//   var $statsComponent = function(blog) {
+//     var component = $('<div>');
+//     component.append([
+//       $headline,
+//       $numberOfArticles(blog),
+//       $numberOfAuthors(blog),
+//       $numberOfWords(blog)
+//     ]);
+//     return component;
+//   };
+//
+//   function renderStats(blog) {
+//     $('#stats').replaceWith($statsComponent(blog));
+//   }
+//
+//   function renderError(message, xhr) {
+//     $('#stats').replaceWith($('<p>Error: <code>' + message + xhr + '</code></p>'));
+//   }
+//
+//   // imperative shell
+//
+//   function stats(data, message, xhr) {
+//     if (xhr.status != 200) {
+//       renderError(message, xhr);
+//     } else {
+//       renderStats(data);
+//     }
+//   }
+//
+//   $.getJSON('scripts/hackerIpsum.json', stats);
+//
+// });
