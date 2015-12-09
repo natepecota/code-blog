4af0a2f15d55d957e4fcff92edbdb70a6237fd6b
natepecota <nate.pecota@gmail.com>
2015-12-08 17:01:00 -0800

    Used the class code that Brook made available on Github and suggested we use to complete/fix/refactor our blogs to be current up through assignment 5 with some work on assignment 7 from today.

--- scripts/article.js
+++ scripts/article.js
@@ -1,36 +1,23 @@
-var Article = function(props) {
-  this.title = props.title;
-  this.category = props.category;
+function Article (props) {
   this.author = props.author;
   this.authorUrl = props.authorUrl;
+  this.title = props.title;
+  this.category = props.category;
+  this.markdown = props.markdown;
+  this.body = props.body || marked(this.markdown);
   this.publishedOn = props.publishedOn;
-  this.body = props.body;
-};
+}
 
-Article.prototype.toHTML = function() {
-  var $art = $('article').first().clone();
-  $art.find('.title').html(this.title);
-  $art.find('.author').html(this.author);
-  $art.find('.body').html(this.body);
-  $art.find('.category').html(this.category);
-  $art.find('.publishedOn').html(this.publishedOn);
-  $art.find('.authorUrl').html(this.authorUrl);
-  $('main').append($art);
-};
+Article.prototype.toHtml = function() {
+  var sourceText = $('#article-template').text();
+  var template = Handlebars.compile(sourceText);
 
-Article.prototype.createFilters = function() {
-  var $authorMenuItemClone = $('.authorMenuItem').clone();
-  $authorMenuItemClone.removeAttr('class');
-  $authorMenuItemClone.attr('value', this.author);
-  $authorMenuItemClone.text(this.author);
-  if ($('#authorSelect').find('option[value="' + this.author + '"]').length === 0) {
-    $('#authorSelect').append($authorMenuItemClone);
-  }
-  var $catMenuItemClone = $('.catMenuItem').clone();
-  $catMenuItemClone.removeAttr('class');
-  $catMenuItemClone.attr('value', this.category);
-  $catMenuItemClone.text(this.category);
-  if ($('#catSelect').find('option[value="' + this.category + '"]').length === 0) {
-    $('#catSelect').append($catMenuItemClone);
-  }
+  this.daysAgo =
+   parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
+
+  this.publishStatus = this.publishedOn ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
+  this.authorSlug = util.slug(this.author);
+  this.categorySlug = util.slug(this.category);
+
+  return template(this);
 };
