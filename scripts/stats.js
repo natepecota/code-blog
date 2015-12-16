d0ce132bb53c49b2b2fecbc9480adbffc951973b
natepecota <nate.pecota@gmail.com>
2015-12-16 13:29:17 -0800

    Add server file and package json files.

--- scripts/stats.js
+++ scripts/stats.js
@@ -1,3 +1,50 @@
+var stats = {};
+stats.uniqueAuthors = [];
+stats.markdownWordCouts = [];
+stats.allWordsArray = [];
+stats.eachAuthorWords = [];
+
+stats.getAllArticles = function(rawJSON) {
+  stats.allArticles = rawJSON;
+};
+
+stats.numberOfArticles = function() {
+  var articleCount = stats.allArticles.length;
+  $('#main').append('<p> Number of Articles: ' + articleCount + '</p>');
+};
+
+stats.searchArticles = function() {
+  var getAuthors = function(art) {
+    if($.inArray(art.author, stats.uniqueAuthors) === -1) {
+      stats.uniqueAuthors.push(art.author);
+    }
+  };
+  stats.allArticles.map(getAuthors);
+};
+
+stats.numberOfAuthors = function() {
+  var authorCount = stats.uniqueAuthors.length;
+  $('#stats').append('<p>Number of Authors ' + authorCount + '</p>');
+};
+
+stats.wordCount = function(str) {
+  return str.replace(/[#,\n]/g, ' ').match(/\b\w+/g);
+};
+
+stats.totalWordCount = function() {
+  var allWords = function(art) {
+    stats.markdownWordCouts.push(stats.wordCount(art.markdown).length);
+  };
+  stats.allArticles.map(allWords);
+  stat.blogWordCount = stats.markdownWordCouts.reduce(getSum, 0);
+  $('#stats').append('<p>Number of Words in All Articles ' + stats.blogWordCount + '</p>');
+};
+
+
+
+
+
+
 // //stats object
 // var stats = {};
 // var statData = '';
@@ -16,20 +63,34 @@
 //
 // stats.getData();
 
-var stats = {};
-​
-$.get('scripts/hackerIpsum.json', function(data) {
-  stats.data = data; //this saves the entire json file as an array that belongs to your stats object, that you can access anywhere
-}).done(stats.numberOfArticles)
-  .done(stats.numberOfAuthors);
-};
-​
-$(function() {
-  stats.data.forEach( function(article) {
-    words = article.body.split(" ")
-	console.log(words)
-  })
-});
+
+
+
+
+
+
+// var stats = {};
+// ​
+// $.get('scripts/hackerIpsum.json', function(data) {
+//   stats.data = data; //this saves the entire json file as an array that belongs to your stats object, that you can access anywhere
+// }).done(stats.numberOfArticles)
+//   .done(stats.numberOfAuthors);
+// };
+// ​
+// $(function() {
+//   stats.data.forEach( function(article) {
+//     words = article.body.split(" ")
+// 	console.log(words)
+//   })
+// });
+
+
+
+
+
+
+
+
 
 // $(function() {
 //
